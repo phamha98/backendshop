a53f38730b711290087ef072e6648639e6afee3b
phamha98 <phamtranquangha@gmail.com>
2021-09-11 01:11:09 +0700

    ht2

--- app/Permission.php
+++ app/Permission.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     //
-    protected $table="permissions";
-     
+    protected $table = "permissions";
+    protected $table3 = "role_permission";
 }
--- app/Role.php
+++ app/Role.php
@@ -8,4 +8,6 @@ class Role extends Model
 {
     //
     protected $table="roles";
+    protected $table2="role_user";
+    protected $table3="role_permission";
 }
